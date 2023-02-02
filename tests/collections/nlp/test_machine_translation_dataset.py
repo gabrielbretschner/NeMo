a3cec497f4303c8ca1722eea2afe356fb3c33655
Gabriel Bretschner <gabriel@lilt.com>
2023-02-02 11:12:28 +0100

    add multiple examples

--- tests/collections/nlp/test_machine_translation_dataset.py
+++ tests/collections/nlp/test_machine_translation_dataset.py
@@ -1,14 +1,22 @@
+import pytest
+
 from nemo.collections.nlp.data import TranslationDataset
 
 
-def test_pack_data_into_batches():
-    tokens_in_batch = 8192
+@pytest.mark.parametrize("segment_length, num_segments, tokens_in_batch, num_expected_batches", [
+    (4096, 2, 8192, 2),
+    (512, 8, 8192, 1),
+    (512, 9, 8192, 2),
+    (1, 8192, 8192, 2),
+])
+def test_pack_data_into_batches(segment_length, num_segments, tokens_in_batch, num_expected_batches):
     dataset = TranslationDataset("", "", tokens_in_batch=tokens_in_batch)
     dataset.src_pad_id = 0
     dataset.tgt_pad_id = 0
-    src = [[1 for _ in range(4096)]] * 2
-    tgt = [[1 for _ in range(4096)]] * 2
+    src = [[1 for _ in range(segment_length)]] * num_segments
+    tgt = [[1 for _ in range(segment_length)]] * num_segments
     batches = dataset.pack_data_into_batches(src, tgt)
+    assert len(batches) == num_expected_batches
     padded_batch = dataset.pad_batches(src, tgt, batches)
     for i, batch in padded_batch.items():
         src_batch_size = batch['src'].size
